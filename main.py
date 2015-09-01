cd356b4973018f8b16d680fd528da4d491527ec9
NurAfeeqah <nurafeeqah13291@yahoo.com>
2015-09-01 06:58:03 +0100

    Initialize brightest path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from motor import Motor
 from sensor_light import Light
 from sensor_ultrasonic import Ultrasonic
+from sensor_camera import Camera
 from bcm_pin import BCM_PIN
 import os
 import time
@@ -16,27 +17,21 @@ class Main:
         self.motor = Motor(50)
         self.light = Light()
         self.ultrasonic = Ultrasonic()
+        self.camera = Camera()
 
-    def start(self):
+    def start(self, condition=1):
+
+        switcher = {
+            0: 'brightest',
+            1: 'distance'
+        }
+
+        method = getattr(self, switcher[condition], lambda: 'nothing')
 
         while(True):
             try:
-                os.system('clear')
-                print "Info: ", datetime.datetime.now()
-                print ""
-                print "Light sensor: "
-                print self.light.getSensorState(1)
-                print self.light.getSensorState(2)
-                print ""
-                print "Ultrasonic Distance: " + str(self.ultrasonic.getDistance()) + "cm"
-
-                if self.ultrasonic.getDistance() > 15:
-                    self.motor.forward()
-                elif self.ultrasonic.getDistance() < 5:
-                    self.motor.reverse()
-                else:
-                    self.motor.stop()
-
+                self.printInfoStatus()
+                method()
                 time.sleep(0.2)
 
             except(KeyboardInterrupt):
@@ -48,11 +43,49 @@ class Main:
                 print "Unexpected error"
                 raise
 
+    def printInfoStatus(self):
+        os.system('clear')
+        print "Info: ", datetime.datetime.now()
+        print ""
+        print "Light sensor: "
+        print self.light.getSensorState(1)
+        print self.light.getSensorState(2)
+        print ""
+        print "Ultrasonic Distance: " + str(self.ultrasonic.getDistance()) + "cm"
+
+    def distance(self):
+        if self.ultrasonic.getDistance() > 15:
+            self.motor.forward()
+        elif self.ultrasonic.getDistance() < 5:
+            self.motor.reverse()
+        else:
+            self.motor.stop()
+
+    def brightest(self):
+        self.camera.captureImage()
+        width, height = self.camera.resolution
+
+        brightest = self.camera.getBrightest()
+
+        # If brightest in left
+        if brightest['x'] < ((width/2) - 25):
+            self.motor.spinanticlockwise()
+
+        # If brightest in right
+        elif brightest['x'] > ((width/2) + 25):
+            self.motor.spinclockwise()
+        
+        # If brightest in front
+        else:
+            self.motor.forward()
+
+        time.sleep(1)
+
     def stop(self):
         self.motor.stop()
         self.bcm_pin.cleanup()
 
 
 main = Main()
-main.start()
+main.start(0)
 
---
+++ sensor_camera.py
@@ -0,0 +1,58 @@
+import picamera
+import time
+import os
+from PIL import Image
+
+class Camera:
+
+    def __init__(self, filename='images/snap.jpg', resolution=(300, 225)):
+        self.filename = filename
+        self.resolution = resolution
+
+    def captureImage(self):
+
+        # Remove existing image first
+        # self.removeImage(self.filename)
+
+        camera = picamera.PiCamera()
+        camera.resolution = self.resolution
+        # camera.start_preview()
+        camera.capture(self.filename)
+        camera.close()
+
+        print "Image has been captured."
+
+    def removeImage(self, filename):
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
+
+    # Return dictionary
+    def getBrightest(self):
+
+        pic = Image.open(self.filename)
+        pic = pic.convert('RGB')
+
+        width, height = self.resolution
+        brightest   = 0.0
+        brightest_x = 0.0
+        brightest_y = 0.0
+        sleep_time = 2
+
+        for y in range(height):
+            for x in range(width):
+                pixelRGB = pic.getpixel((x,y))
+                R,G,B = pixelRGB
+                brightness = (0.3*R) + (0.59*G) + (0.11*B)
+                if brightness > brightest:
+                    brightest = brightness
+                    brightest_x = x
+                    brightest_y = y
+
+        return {
+            'x'   : brightest_x,
+            'y'   : brightest_y,
+            'val' : brightest
+        }
+        
